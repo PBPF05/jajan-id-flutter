28b5f2c1d82cd3242dd8c3bedf48685dc5aa680c
Ren <renrror@gmail.com>
2022-12-10 09:28:26 +0700

    Add pull down to refresh

--- lib/screens/chat/list.dart
+++ lib/screens/chat/list.dart
@@ -15,58 +15,66 @@ class ChatListPage extends StatefulWidget {
 class _ChatListPageState extends State<ChatListPage> {
   late Future<ChannelData> chatsFuture;
 
-  Future<ChannelData> fetchChats(AppRequest request) async {
+  Future<ChannelData> fetchChats() async {
+    var request = context.read<AppRequest>();
     Map<String, dynamic> res = await request.get("chat/get");
     return ChannelData.fromJson(res);
   }
 
   @override
-  Widget build(BuildContext context) {
-    final request = context.read<AppRequest>();
+  void initState() {
+    super.initState();
+    chatsFuture = fetchChats();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Chat"),
       ),
       drawer: const AppDrawer(),
-      body: FutureBuilder(
-        future: fetchChats(request),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  CircularProgressIndicator(),
-                  Text("Loading..."),
-                ],
-              ),
-            );
-          }
+      body: RefreshIndicator(
+        onRefresh: fetchChats,
+        child: FutureBuilder(
+          future: chatsFuture,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text("Loading..."),
+                  ],
+                ),
+              );
+            }
 
-          if (snapshot.hasError) {
-            return Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.warning,
-                    color: Colors.yellow,
-                    size: 30.0,
-                  ),
-                  Text("An error has occured")
-                ],
-              ),
-            );
-          }
+            if (snapshot.hasError) {
+              return Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.warning,
+                      color: Colors.yellow,
+                      size: 30.0,
+                    ),
+                    Text("An error has occured")
+                  ],
+                ),
+              );
+            }
 
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ChatListing(data: snapshot.data!),
-          );
-        },
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ChatListing(data: snapshot.data!),
+            );
+          },
+        ),
       ),
     );
   }
